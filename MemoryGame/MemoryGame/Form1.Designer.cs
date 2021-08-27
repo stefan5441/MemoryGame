ed1d9c3f9664e0f8cde4bd8de39a6466203973b7
ValentinoTrifunovski <valentino.trifunovski@gmail.com>
2021-08-27 22:53:20 +0200

    Quit button

--- MemoryGame/MemoryGame/Form1.Designer.cs
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace MemoryGame
             this.btnEasy = new System.Windows.Forms.Button();
             this.btnMedium = new System.Windows.Forms.Button();
             this.btnHard = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEasy
@@ -60,15 +61,26 @@ namespace MemoryGame
             this.btnHard.Name = "btnHard";
             this.btnHard.Size = new System.Drawing.Size(94, 29);
             this.btnHard.TabIndex = 2;
-            this.btnHard.Text = "Hard";
+            this.btnHard.Text = "Тешко";
             this.btnHard.UseVisualStyleBackColor = true;
             this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnHard);
             this.Controls.Add(this.btnMedium);
             this.Controls.Add(this.btnEasy);
@@ -83,6 +95,7 @@ namespace MemoryGame
         private System.Windows.Forms.Button btnEasy;
         private System.Windows.Forms.Button btnMedium;
         private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Button button1;
     }
 }
 
