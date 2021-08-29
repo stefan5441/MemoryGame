f3e20dd94b8bbfc27d2d291983ae8a2f535786cd
ValentinoTrifunovski <valentino.trifunovski@gmail.com>
2021-08-29 20:22:03 +0200

    menu

--- MemoryGame/MemoryGame/Form1.Designer.cs
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -37,37 +37,38 @@ namespace MemoryGame
             // 
             // btnEasy
             // 
-            this.btnEasy.Location = new System.Drawing.Point(283, 62);
+            this.btnEasy.Location = new System.Drawing.Point(334, 161);
             this.btnEasy.Name = "btnEasy";
-            this.btnEasy.Size = new System.Drawing.Size(94, 29);
+            this.btnEasy.Size = new System.Drawing.Size(254, 51);
             this.btnEasy.TabIndex = 0;
-            this.btnEasy.Text = "button1";
-            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Text = "Easy Mode";
+            this.btnEasy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnEasy.UseVisualStyleBackColor = false;
             this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
             // 
             // btnMedium
             // 
-            this.btnMedium.Location = new System.Drawing.Point(283, 150);
+            this.btnMedium.Location = new System.Drawing.Point(334, 234);
             this.btnMedium.Name = "btnMedium";
-            this.btnMedium.Size = new System.Drawing.Size(94, 29);
+            this.btnMedium.Size = new System.Drawing.Size(254, 53);
             this.btnMedium.TabIndex = 1;
-            this.btnMedium.Text = "Medium";
+            this.btnMedium.Text = "Medium Mode";
             this.btnMedium.UseVisualStyleBackColor = true;
             this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
             // 
             // btnHard
             // 
-            this.btnHard.Location = new System.Drawing.Point(283, 255);
+            this.btnHard.Location = new System.Drawing.Point(334, 316);
             this.btnHard.Name = "btnHard";
-            this.btnHard.Size = new System.Drawing.Size(94, 29);
+            this.btnHard.Size = new System.Drawing.Size(254, 57);
             this.btnHard.TabIndex = 2;
-            this.btnHard.Text = "Тешко";
+            this.btnHard.Text = "Hard Mode";
             this.btnHard.UseVisualStyleBackColor = true;
             this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(682, 398);
+            this.btnQuit.Location = new System.Drawing.Point(808, 451);
             this.btnQuit.Name = "btnQuit";
             this.btnQuit.Size = new System.Drawing.Size(94, 29);
             this.btnQuit.TabIndex = 3;
@@ -79,11 +80,13 @@ namespace MemoryGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(926, 503);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.btnHard);
             this.Controls.Add(this.btnMedium);
             this.Controls.Add(this.btnEasy);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
